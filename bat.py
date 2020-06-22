9ed76a04dec722a5f22b6193546e843744d9b5d4
jefferson-calmon <62778628+jefferson-calmon@users.noreply.github.com>
2020-06-22 20:28:21 -0300

    teste

--- bat.py
+++ bat.py
@@ -1,10 +1,17 @@
 import os
 import time
 
-os.system('bat.bat')
-time.sleep(5)
+def commit():
+    os.system('git add . > null')
+    os.system('git commit -m "teste"')
 
-os.system('jefferson-calmon')
-time.sleep(3)
+def editFile(id):
+    with open('README.md', 'a') as file:
+        file.write(f'Teste Planejado`{id}')
+        commit()
 
-os.system('Je10?200')
+number = int(input('>>'))
+ind = 0
+
+while ind < number:
+    editFile(ind)
