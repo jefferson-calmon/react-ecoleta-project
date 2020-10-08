2186db929e5fcc4dc33f3a628561985752889fe6
Jefferson Calmon <jeffjeff0197@gmail.com>
2020-10-07 22:00:29 -0300

    teste

---
+++ bat.py
@@ -0,0 +1,19 @@
+import os
+import time
+
+def commit():
+    os.system('git add . > null')
+    os.system('git commit -m "teste"')
+
+def editFile(id):
+    with open('README.md', 'a') as file:
+        file.write(str(id))
+        commit()
+
+number = int(input('>>'))
+ind = 0
+
+while ind < number:
+    editFile(ind)
+    ind += 1
+os.system('git push')
